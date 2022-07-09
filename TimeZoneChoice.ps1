4466746f5805a281d948a2345ba260ca2c216502
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-07-09 05:56:09 -0800

    Create TimeZoneChoice.ps1

---
+++ TimeZoneChoice.ps1
@@ -0,0 +1,12 @@
+# Set Time Zone from a list
+
+$opt1 = "eastern standard time"
+$opt2 = "central standard time"
+$opt3 = "mountain standard time"
+$opt4 = "pacific standard time"
+
+$opt5 = "pacific daylight time"
+$opt6 = "pacific daylight time"
+$opt7 = "pacific daylight time"
+$opt8 = "pacific daylight time"
+
