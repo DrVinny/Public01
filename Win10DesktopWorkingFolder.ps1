f65ea0f7aa56d25fd9ee4a73229696d02509e713
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-07-14 10:11:43 -0600

    Update Win10DesktopWorkingFolder.ps1

--- Win10DesktopWorkingFolder.ps1
+++ Win10DesktopWorkingFolder.ps1
@@ -1,4 +1,4 @@
-# Create desktop working ("w") folder and subfolders in current profile
+# This is a sample of how to create folders, in this case on the desktop, but can be adapted to work in any path
 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 $topFolder = "w"
