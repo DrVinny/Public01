b652f874da53fbe0980c285fe023c4762ddd836f
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-07-16 11:49:34 -0600

    Create Win10GetLatestAppInstaller.ps1

---
+++ Win10GetLatestAppInstaller.ps1
@@ -0,0 +1,2 @@
+# This file gets the latest version of the App Installer, which is required for 'winget' to work.
+# It is already updated in later versions of windows 10 and all versions of windows 11, but may not come out of the box for earlier versions of windows 10
