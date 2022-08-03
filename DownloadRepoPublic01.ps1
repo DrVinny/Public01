4e9282ca70b39e85e91ca7f58bd9302beafa73af
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-08-03 09:44:49 -0600

    Create DownloadRepoPublic01.ps1

---
+++ DownloadRepoPublic01.ps1
@@ -0,0 +1,16 @@
+# This script uses powershell to download the Publc01 Repo
+
+$repoOwn = 'DrVinny'
+$repoName = 'Public01'
+
+# $repoBranch = 'main'
+# $repoURI = "https://api.github.com/repos/$repoOwn/$repoName/zipball/$repoBranch"
+# Main is assumed if $repoBranch is omitted
+
+$repoURI = "https://api.github.com/repos/$repoOwn/$repoName/zipball/"
+# $token = "ghp_yourAccessToken"
+# $headers = @{"Authorization" = "bearer $token"}
+
+$ZipFile = "$pwd\$repoName.zip"
+
+Invoke-RestMethod -Uri $repoURI -OutFile $ZipFile -Method Get # -Headers $headers
