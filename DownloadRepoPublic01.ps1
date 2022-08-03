6ca098b7b001ee1690e935742c1de45eaa63e55b
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-08-03 11:38:22 -0600

    Update DownloadRepoPublic01.ps1

--- DownloadRepoPublic01.ps1
+++ DownloadRepoPublic01.ps1
@@ -6,6 +6,7 @@ $repoName = 'Public01'
 # $repoBranch = 'main'
 # $repoURI = "https://api.github.com/repos/$repoOwn/$repoName/zipball/$repoBranch"
 # Main is assumed if $repoBranch is omitted
+# No token is required for public Repos
 
 $repoURI = "https://api.github.com/repos/$repoOwn/$repoName/zipball/"
 # $token = "ghp_yourAccessToken"
