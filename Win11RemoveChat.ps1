8f547ca3dfa50d614d4539bdbdd0b65890ea84b4
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-08-04 14:55:07 -0600

    pasted actual script contents

--- Win11RemoveChat.ps1
+++ Win11RemoveChat.ps1
@@ -1,1 +1,31 @@
+# Manage chat in windows 11
+# Source: https://community.spiceworks.com/topic/2268367-powershell-check-if-registry-item-exists-if-not-create
 
+
+# This step removes chat from the taskbar
+
+$tbChatPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$tbChatName = "TaskbarMn"
+$tbChatValue = "0"
+
+$tbChatTest = gpv $tbChatPath -Name $tbChatName
+
+if ($tbChatTest -eq 0) {
+    Write-Output "Value Exists and is already set to 0"
+}
+else {
+    Set-ItemProperty -Path $tbChatPath -Name $tbChatName -Value $tbChatValue
+}
+
+# This step removes chat toggle from taskbar settings
+
+<#
+$settingsChatPath = ""
+$settingsChatName = ""
+$settingsChatValue = ""
+
+
+Set-ItemProperty -Path $settingsChatPath -Name $settingsChatName -Value $settingsChatValue
+#>
+
+Stop-Process -Name explorer
