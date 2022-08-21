1045895354ca285885bd1ba005d766bf49fb1962
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-08-21 09:26:05 -0600

    Update PSGetPublicIP.ps1

--- PSGetPublicIP.ps1
+++ PSGetPublicIP.ps1
@@ -1,4 +1,6 @@
 # This script gets the public ip address of the current network and saves it to a variable
 
 $myPubIP = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+$myPubIP | set-clipboard
 $myPubIP
+
