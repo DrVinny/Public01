b1a0a8a1d00df55202112bddd246fed13834c78c
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-09-04 13:30:15 -0600

    added history file delete command and sources

--- PSClearHistoryBuffer.ps1
+++ PSClearHistoryBuffer.ps1
@@ -1,1 +1,15 @@
+# Clear session buffer
+# Source: https://stackoverflow.com/questions/13257775/powershells-clear-history-doesnt-clear-history
 ([Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory())
+
+# Remove history file
+# Source: https://www.shellhacks.com/clear-history-powershell/
+
+Remove-Item (Get-PSReadlineOption).HistorySavePath
+
+# Change how history is saved:
+# Source: https://www.shellhacks.com/clear-history-powershell/
+
+# PS C:\> Set-PSReadlineOption -HistorySaveStyle SaveIncrementally # default
+# PS C:\> Set-PSReadlineOption -HistorySaveStyle SaveAtExit
+# PS C:\> Set-PSReadlineOption -HistorySaveStyle SaveNothing
