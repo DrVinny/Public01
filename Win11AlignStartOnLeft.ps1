547e9e1a24e78409b55a09a2dd668aca49f030ce
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-09-20 08:16:07 -0600

    added elseif

--- Win11AlignStartOnLeft.ps1
+++ Win11AlignStartOnLeft.ps1
@@ -1,17 +1,21 @@
 # In Windows 11, set the start button to be aligned to the left
-$keyVar = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$nameVar = "TaskbarAl"
-$valueVar = "0"
+$keyAl = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$nameAl = "TaskbarAl"
+# Left Aligned
+$valueAl = "0"
+# Center Aligned
+# $valueAl = "1"
 
 # Run a check to see if the value exists and save to $checkResult
-$checkResult = (gp $keyVar).$nameVar
+$checkResult = (gp $keyAl).$nameAl
 
-if ($checkResult -eq $null){
-    
-#     New-Item -Path $keyVar -Force | Out-Null
-
-    New-ItemProperty -Path $keyVar -Name $nameVar -Value $valueVar -PropertyType DWORD -Force | Out-Null
-
+if ($checkResult -eq $null){    
+    # Inculuding the one line immediately below will reset the entire $keyAL key
+    # New-Item -Path $keyAl -Force | Out-Null
+    New-ItemProperty -Path $keyAl -Name $nameAl -Value $valueAl -PropertyType DWORD -Force | Out-Null
+}
+elseif ($checkResult -eq 1) {
+    Set-ItemProperty -Path $keyAl -Name $nameAl -Value "0"
 }
 else {
     Write-Output "Registry entry already exists, and is already set to the correct value (0)."
