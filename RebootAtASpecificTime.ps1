ce18a0ba7da1087d872fd37d457cd2e82b465b29
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-12-29 11:08:44 -0700

    Update RebootAtASpecificTime.ps1
    shortened script
    updated notes to be more concise

--- RebootAtASpecificTime.ps1
+++ RebootAtASpecificTime.ps1
@@ -1,14 +1,18 @@
-# log into a pssession
-# disconnect but leave session open
+# This does not work in a PSSession
+# Must be on a console directly on the machine, (rdp or ninja), or run as a ps1 file
 
-# then use "New-TimeSpan"
-# Source: https://devblogs.microsoft.com/scripting/powertip-get-time-difference-between-dates-with-powershell/
-# Source 2: https://www.reddit.com/r/PowerShell/comments/bddl8z/restartcomputer_at_a_certain_time/
+# save a .ps1 file with the following contents, change time as necessary
+# double check that $waitseconds echo is a positive number
+# negative number indicates that there is likely a typo or incorrect date
 
-$startTime = get-date
-# change date to match current reboot requirements
-$rebootTime = get-Date -Date 2022-12-2T08:50
+$startTime = Get-Date
+$rebootTime = Get-Date -Date 2022-12-29t09:14
 $waitSeconds = ($rebootTime-$startTime).totalSeconds
 $waitSeconds
+start-sleep -seconds $waitSeconds
 
-shutdown -r -t $waitseconds
+restart-computer -force
+
+
+# Source: https://devblogs.microsoft.com/scripting/powertip-get-time-difference-between-dates-with-powershell/
+# Source 2: https://www.reddit.com/r/PowerShell/comments/bddl8z/restartcomputer_at_a_certain_time/
