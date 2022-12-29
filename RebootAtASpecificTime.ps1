6466c5211293c9b81db47e56fa33f2a979163165
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2022-12-29 08:47:01 -0700

    Update RebootAtASpecificTime.ps1
    add echo wait seconds

--- RebootAtASpecificTime.ps1
+++ RebootAtASpecificTime.ps1
@@ -1,13 +1,14 @@
-log into a pssession
-disconnect but session open
+# log into a pssession
+# disconnect but leave session open
 
-then use "New-TimeSpan"
+# then use "New-TimeSpan"
 # Source: https://devblogs.microsoft.com/scripting/powertip-get-time-difference-between-dates-with-powershell/
 # Source 2: https://www.reddit.com/r/PowerShell/comments/bddl8z/restartcomputer_at_a_certain_time/
 
 $startTime = get-date
 # change date to match current reboot requirements
-$rebootTime = get-Date -Date 2022-12-28T20:00
-$waitSeconds = ($startTime - $rebootTime).totalSeconds
+$rebootTime = get-Date -Date 2022-12-2T08:50
+$waitSeconds = ($rebootTime-$startTime).totalSeconds
+$waitSeconds
 
 shutdown -r -t $waitseconds
