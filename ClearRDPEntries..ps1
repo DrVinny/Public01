752bd47c50748ad18b39097927c0d51adbaebfcf
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2023-02-03 10:59:35 -0700

    Create ClearRDPEntries..ps1
    base script created, based on windows CMD commands
    powershell commands not yet added

---
+++ ClearRDPEntries..ps1
@@ -0,0 +1,16 @@
+# Source: https://www.thewindowsclub.com/remote-desktop-connection-clear-history
+
+
+
+
+
+# CMD commands block below
+<#
+@echo off
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" /va /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers"
+cd %userprofile%\documents\
+attrib Default.rdp -s -h
+del Default.rdp
+#>
