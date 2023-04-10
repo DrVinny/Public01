07f9076d18cc939e0fc3a8de606cfb6dacb975ed
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2023-04-10 08:15:51 -0600

    change to Enable-WindowsOptionalFeature options

--- InstallHyperVTools.ps1
+++ InstallHyperVTools.ps1
@@ -1,4 +1,19 @@
 # Source: https://learn.microsoft.com/en-us/windows-server/virtualization/hyper-v/manage/remotely-manage-hyper-v-hosts
+# the original source, but: *only works if you have RSAT installed
+
+# add-windowsfeature rsat-hyper-v-tools
 
 
-add-windowsfeature rsat-hyper-v-tools
+# Instead try this:
+# https://technoresult.com/how-to-turn-on-or-off-windows-features-using-powershell/
+# Get-WindowsOptionalFeature –Online -FeatureName *hyper*
+# give you:
+
+# Pick one
+# I think its this one
+Enable-WindowsOptionalFeature -Online -FeatureName “Microsoft-Hyper-V-Tools-All” –all
+
+# but could be
+# Enable-WindowsOptionalFeature -Online -FeatureName “Microsoft-Hyper-V-All” –all
+# or
+# Enable-WindowsOptionalFeature -Online -FeatureName “Microsoft-Hyper-V” –all
