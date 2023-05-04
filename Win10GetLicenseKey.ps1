ab37df4397870e63438703a4827c30b4f0383658
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2023-05-04 14:25:22 -0600

    Update Win10GetLicenseKey.ps1

--- Win10GetLicenseKey.ps1
+++ Win10GetLicenseKey.ps1
@@ -1,11 +1,12 @@
 # Source: 
 # https://www.techrepublic.com/article/3-simple-ways-to-find-your-windows-10-product-key/
-# (Use the 3rd option)
+# (this script uses the 3rd option)
 
 # Check if there is a property under the value in the following path
 
+
 $licKey = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform"
 $licVal = "BackupProductKeyDefault"
-# $licProp
 
-Test-Path $licKey
+$licCheck = (Get-ItemProperty $licKey).$licVal
+$licCheck
