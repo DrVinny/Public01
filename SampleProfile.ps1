45fe97705a39cbf941a398f51e50bab8f2396b80
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2023-09-07 19:49:41 -0600

    Update SampleProfile.ps1
    added profile dir / file creation

--- SampleProfile.ps1
+++ SampleProfile.ps1
@@ -1,3 +1,9 @@
+# Create profile folder / file
+<#
+$profDir = Split-path $PROFILE
+ni $profDir -ItemType Directory
+ni $PROFILE
+#>
 # Clear History buffer
 
 ([Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory())
