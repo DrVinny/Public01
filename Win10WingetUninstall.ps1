623b7a14781e810142906dc3b9228b9e38ffd117
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2023-09-07 19:36:11 -0600

    Added missing commas in program list
    Added missing commas in program list

--- Win10WingetUninstall.ps1
+++ Win10WingetUninstall.ps1
@@ -31,11 +31,11 @@ $uninstApps =
 "Microsoft.Xbox.TCUI_8wekyb3d8bbwe",
 "Microsoft.Getstarted_8wekyb3d8bbwe",
 "Microsoft.MicrosoftEdge.Stable_8wekyb3d8bbwe",
-"Microsoft.EdgeWebView2Runtime"
-"9WZDNCRFJ3Q2" # MSN Weather, but it doesn't work
+"Microsoft.EdgeWebView2Runtime",
+"9WZDNCRFJ3Q2", # MSN Weather, but it doesn't work
 
 # This will uninstall windows 11 widgets, among other things
-"MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy"
+"MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy",
 # Just in case the Above doesn't work
 "windows web experience pack"
 
