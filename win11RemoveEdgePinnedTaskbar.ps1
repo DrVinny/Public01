01d24e445df8790cda9e81781d5ee8be5123cc9e
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2024-03-18 09:08:10 -0600

    Cleaned up
    Removed the old code and simplified to just removing the .lnk file from the shortcut from the taskbar folder.

--- win11RemoveEdgePinnedTaskbar.ps1
+++ win11RemoveEdgePinnedTaskbar.ps1
@@ -1,10 +1,8 @@
 # Source 1: https://superuser.com/questions/984091/remove-microsoft-edge-and-store-taskbar-shortcuts-for-all-new-users-windows-10
 # Source 2: https://stackoverflow.com/questions/45152335/unpin-the-microsoft-edge-and-store-taskbar-shortcuts-programmatically
 
-<# The link is found in : 
- %AppData%\Microsoft\Internet Explorer\Quick Launch\
- and 
- %AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar,
+<# The link shortcut is found in : 
+$env:AppData\"Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
 remove all instances and variances of "Microsoft Edge.lnk"
  #>
 
@@ -14,10 +12,10 @@ HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband
 It may be easier to just remove the .lnk files
 #>
 
-function Unpin-App([string]$appname) {
-    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() |
-        ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt()}
-}
-
-Unpin-App("Microsoft Edge")
-Unpin-App("Microsoft Store")
+# List pinned apps
+ls $env:APPDATA\"Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+# See if edge is there
+Test-Path $env:APPDATA\"Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
+# Remove it
+Remove-Item $env:APPDATA\"Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
+Stop-Process -Name explorer
